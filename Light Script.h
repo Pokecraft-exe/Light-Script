64debd286cd2c071b53364f2188fc9054f88f56a
Julian LF <67156699+Pokecraft-exe@users.noreply.github.com>
2022-08-29 22:13:17 +0200

    adding type <any> and fixed errors

--- Light Script.h
+++ Light Script.h
@@ -6,15 +6,186 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <utility>
 #include <cstdlib>
+#include <type_traits>
+#include <utility>
+#include <typeinfo>
+#include <cassert>
+#include <bitset>
 using namespace std;
 
-typedef vector<pair<pair<int,float>, pair<string, vector<pair<pair<int,float>, string> > > > > list; //used
-typedef pair<pair<int,float>, pair<string, list> > any; // used
-typedef map<string, any> dict; // used
+template <class T>
+using StorageType = typename decay<T>::type; 
+struct Any
+{
+    bool is_null() const { return !ptr; }
+    bool not_null() const { return ptr; }
 
-string any_to_string(any any_);
+    template<typename U> Any(U&& value)
+        : ptr(new Derived<StorageType<U>>(forward<U>(value)))
+    {
+
+    }
+
+    template<class U> bool is() const
+    {
+        typedef StorageType<U> T;
+
+        auto derived = dynamic_cast<Derived<T>*> (ptr);
+
+        return derived;
+    }
+
+    template<class U>
+    StorageType<U>& as()
+    {
+        typedef StorageType<U> T;
+
+        auto derived = dynamic_cast<Derived<T>*> (ptr);
+
+        if (!derived)
+            throw bad_cast();
+
+        return derived->value;
+    }
+
+    template<class U>
+    operator U()
+    {
+        return as<StorageType<U>>();
+    }
+
+    Any()
+        : ptr(nullptr)
+    {
+
+    }
+
+    Any(Any& that)
+        : ptr(that.clone())
+    {
+
+    }
+
+    Any(Any&& that)
+        : ptr(that.ptr)
+    {
+        that.ptr = nullptr;
+    }
+
+    Any(const Any& that)
+        : ptr(that.clone())
+    {
+
+    }
+
+    Any(const Any&& that)
+        : ptr(that.clone())
+    {
+
+    }
+
+    Any& operator=(const Any& a)
+    {
+        if (ptr == a.ptr)
+            return *this;
+
+        auto old_ptr = ptr;
+
+        ptr = a.clone();
+
+        if (old_ptr)
+            delete old_ptr;
+
+        return *this;
+    }
+
+    Any& operator=(Any&& a)
+    {
+        if (ptr == a.ptr)
+            return *this;
+
+        swap(ptr, a.ptr);
+
+        return *this;
+    }
+    
+    void* print() {
+    	if (is<int>()) {cout << as<int>();}
+		if (is<float>()) {cout << as<float>();}
+		if (is<string>()) {cout << as<string>();}
+	}
+
+    ~Any()
+    {
+        if (ptr)
+            delete ptr;
+    }
+
+private:
+    struct Base
+    {
+        virtual ~Base() {}
+
+        virtual Base* clone() const = 0;
+    };
+
+    template<typename T>
+    struct Derived : Base
+    {
+        template<typename U> Derived(U&& value) : value(forward<U>(value)) { }
+
+        T value;
+
+        Base* clone() const { return new Derived<T>(value); }
+    };
+
+    Base* clone() const
+    {
+        if (ptr)
+            return ptr->clone();
+        else
+            return nullptr;
+    }
+
+    Base* ptr;
+};
+
+template<std::size_t N>
+bool operator<(const std::bitset<N>& x, const std::bitset<N>& y)
+{
+    for (int i = N-1; i >= 0; i--) {
+        if (x[i] ^ y[i]) return y[i];
+    }
+    return false;
+}
+
+template<std::size_t N>
+bool operator>(const std::bitset<N>& x, const std::bitset<N>& y)
+{
+    for (int i = N-1; i >= 0; i--) {
+        if (y[i] ^ x[i]) return x[i];
+    }
+    return false;
+}
+
+template<std::size_t N>
+bool operator<=(const std::bitset<N>& x, const std::bitset<N>& y)
+{
+    if (x < y || x == y) {return true;}
+    return false;
+}
+
+template<std::size_t N>
+bool operator>=(const std::bitset<N>& x, const std::bitset<N>& y)
+{
+    if (x > y || x == y) {return true;}
+    return false;
+}
+
+typedef vector<Any> list;
+typedef map<string, Any> dict;
+
 void replaceAll(string& s, const string& search, const string& replace);
 string read(string file);
 void write(string file, string towrite);
@@ -33,8 +204,8 @@ bool isfunc(string s);
 bool isfloat(string s);
 bool isint(string s);
 string replacevar(string s, dict var);
-any scanCondType(string after);
-any scanOperator(string after);
+string scanCondType(string after);
+string scanOperator(string after);
 int calculate(string s);
 bool ismath(string str, dict var);
 int findchar(string s, char chr);
@@ -57,10 +228,10 @@ public:
 
     ls(string script);
     void parse();
-    any typescan(string after);
+    Any typescan(string after);
     void scanVarI(string l);
     void scanCondI(string l);
-    any condI(string l, int line);
-    any exec_(string i, int line, list parameters, string function, bool one = 0);
-    any exec(string function, int line);
+    Any condI(string l, int line);
+    Any exec_(string i, int line, list parameters, string function, bool one = 0);
+    Any exec(string function, int line);
 };
